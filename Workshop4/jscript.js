8ef595db3ed157afe4885edb908c85aed1484075
T0m1H <tomi.t.haukka@gmail.com>
2025-03-31 09:06:02 +0300

    workshopin tekoa

---
+++ Workshop4/jscript.js
@@ -0,0 +1,7 @@
+    function changeHeading() {
+        // Find the reference to the "Workshop 4" heading
+        const heading = document.querySelector('h1');
+        
+        // Modify the content of the heading
+        heading.textContent = 'Modified Heading!';
+    }
