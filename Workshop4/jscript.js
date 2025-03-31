cce0572f02a1c45ffdd662269d71f288f5a769b5
T0m1H <tomi.t.haukka@gmail.com>
2025-03-31 09:14:24 +0300

    exercise3

--- Workshop4/jscript.js
+++ Workshop4/jscript.js
@@ -17,3 +17,24 @@ function exercise2() {
     heading.style.backgroundColor = "yellow"; // Change background color
 }
 
+function appendParagraph() {
+    // Locate the 4th <p> element
+    const paragraphs = document.querySelectorAll('p');
+    if (paragraphs.length >= 4) {
+        const fourthParagraph = paragraphs[3]; // Index 3 corresponds to the 4th paragraph
+
+        // Create a new paragraph element
+        const newParagraph = document.createElement('p');
+
+        // Set the text content (including italic styling)
+        newParagraph.innerHTML = '<em>"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed posuere interdum sem. Quisque ligula eros ullamcorper quis, lacinia quis facilisis sed sapien. Mauris varius diam vitae arcu. Sed arcu lectus auctor vitae, consectetuer et venenatis eget velit."</em>';
+
+        // Append the new paragraph after the 4th <p>
+        fourthParagraph.parentNode.insertBefore(newParagraph, fourthParagraph.nextSibling);
+
+        // Optionally, change the background color of the page
+        document.body.style.backgroundColor = '#f0f8ff'; // Light blue
+    }
+}
+
+
